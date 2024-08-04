e84d86fdcac6abf15cfe9186e36d3c4bb48e218e
Diego Tolentino <114973390+tolerevo@users.noreply.github.com>
2024-08-04 17:56:23 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+function App() {
+  const [isRedColor, setRedColor] = React.useState(false);
+  const toggleColor = () => setRedColor((prev) => !prev);
+  return (
+    <button
+      onClick={toggleColor}
+      style={{
+        background: isRedColor ? "red" : "black",
+        color: "white",
+      }}
+    >
+      Button is {isRedColor ? "red" : "not red"}
+    </button>
+  );
+}
+export default App;
